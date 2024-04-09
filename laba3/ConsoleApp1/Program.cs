33ab8c3b08efa727ff852aeeef93cf8dd46a1a8b
mogkolya <95872473+quantum200@users.noreply.github.com>
2024-04-09 12:01:17 +0300

    Update Program.cs

--- laba3/ConsoleApp1/Program.cs
+++ laba3/ConsoleApp1/Program.cs
@@ -1,61 +1,79 @@
-﻿using System;
-class Time
+using System;
+enum TimeOfDay
 {
-    public int Hour { get; set; }// Година
-    public int Minute { get; set; }// Хвилина
-
-    public Time(int hour, int minute)
+    Ранок,
+    День,
+    Вечір,
+    Ніч
+}
+class Timer
+{
+    public int Hours { get; set; }
+    public int Minutes { get; set; }
+    public int Seconds { get; set; }
+}
+class Counter
+{
+    public int Seconds { get; set; }
+    public static implicit operator Counter(int x)// Перетворює ціле число у тип Counter
     {
-        Hour = hour;
-        Minute = minute;
+        return new Counter { Seconds = x };
     }
-    public override bool Equals(object obj)// Перевизначення методу Equals для порівняння об'єктів класу Time
+    public static explicit operator int(Counter counter)// Явне перетворення типу Counter у ціле число
     {
-        if (obj == null || GetType() != obj.GetType())
-        {
-            return false;
-        }
-
-        Time other = (Time)obj;
-        return Hour == other.Hour && Minute == other.Minute;
+        return counter.Seconds;
     }
-    public override int GetHashCode()// Перевизначення методу GetHashCode для отримання хеш-коду об'єкту
+    public static explicit operator Counter(Timer timer)// Явне перетворення типу Timer у тип Counter
     {
-        return Tuple.Create(Hour, Minute).GetHashCode();
+        int h = timer.Hours * 3600;
+        int m = timer.Minutes * 60;
+        return new Counter { Seconds = h + m + timer.Seconds };
     }
-    public override string ToString()// Перевизначення методу ToString для отримання рядкового представлення об'єкту
+    public static implicit operator Timer(Counter counter)// Перетворює тип Counter у тип Timer
     {
-        return $"Час: {Hour}:{Minute}";
+        int h = counter.Seconds / 3600;
+        int m = (counter.Seconds % 3600) / 60;
+        int s = counter.Seconds % 60;
+        return new Timer { Hours = h, Minutes = m, Seconds = s };
     }
 }
-class Day
+class DayTime
 {
-    public Time CurrentTime { get; set; }// Поточний час
-    public Day(Time currentTime)
+    public TimeOfDay GetTimeOfDay(Timer timer)
     {
-        CurrentTime = currentTime;
+        int totalSeconds = timer.Hours * 3600 + timer.Minutes * 60 + timer.Seconds;
+        if (totalSeconds >= 21600 && totalSeconds < 43199) // Утро: з 06:00 до 11:59
+        {
+            return TimeOfDay.Ранок;
+        }
+        else if (totalSeconds >= 43200 && totalSeconds < 61199) // День: з 12:00 до 16:59
+        {
+            return TimeOfDay.День;
+        }
+        else if (totalSeconds >= 61200 && totalSeconds < 86399) // Вечір: з 17:00 до 23:59
+        {
+            return TimeOfDay.Вечір;
+        }
+        else // Ніч: з 00:00 до 05:59
+        {
+            return TimeOfDay.Ніч;
+        }
     }
-    public void PrintCurrentTime()// Метод для виведення поточного часу у консоль
+}
+class Program
+{
+    static void Main(string[] args)
     {
-        Console.WriteLine(CurrentTime.ToString());
-    }
-    public string CalculateTimeOfDay()// Метод для визначення часу доби на основі поточного часу
-    {
-        if (CurrentTime.Hour >= 5 && CurrentTime.Hour < 12)
+        DateTime currentTime = DateTime.Now;
+        Timer timer = new Timer
         {
-            return "Ранок";
-        }
-        else if (CurrentTime.Hour >= 12 && CurrentTime.Hour < 18)
-        {
-            return "День";
-        }
-        else if (CurrentTime.Hour >= 18 && CurrentTime.Hour < 22)
-        {
-            return "Вечір";
-        }
-        else
-        {
-            return "Ніч";
-        }
+            Hours = currentTime.Hour,
+            Minutes = currentTime.Minute,
+            Seconds = currentTime.Second
+        };
+        DayTime dayTimeCalculator = new DayTime();
+        TimeOfDay timeOfDay = dayTimeCalculator.GetTimeOfDay(timer);
+        Console.WriteLine($"Поточний час: {currentTime.Hour}:{currentTime.Minute}:{currentTime.Second}");
+        Console.WriteLine($"Час доби: {timeOfDay}");
     }
 }
